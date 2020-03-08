a02f5b6e6d81989e7eb67c1467602a10ed7f2fcb
kuromogeko <ToteHosen3>
2020-03-08 17:44:27 +0100

    MultiTransform Test

--- src/test/groovy/de/nkm/manualchecker/checks/tasks/ResultSaverActionTest.groovy
+++ src/test/groovy/de/nkm/manualchecker/checks/tasks/ResultSaverActionTest.groovy
@@ -4,6 +4,8 @@ import de.kosit.validationtool.impl.tasks.CheckAction
 import de.nkm.manualchecker.checks.config.TransformationConfiguration
 import spock.lang.Specification
 
+import java.nio.file.Files
+
 class ResultSaverActionTest extends Specification {
     def "it should take a Source and save it"(){
         given: "a bag with a base file"
@@ -73,5 +75,35 @@ class ResultSaverActionTest extends Specification {
 
         then: "The action will be skipped"
         skipped
+    }
+
+    def "it should do a multi transform"(){
+        given: "a bag with a base file"
+        def bag = new CheckAction.Bag()
+        bag.setXsltTransformationTarget(getClass().getResource('/visualization/xml/maxRechnung.xml').getPath())
+        and: "multiple transformations"
+        bag.getXsltSelectionResults().add(getClass().getResource('/visualization/xsl/ubl-invoice-xr.xsl').getPath())
+        bag.getXsltSelectionResults().add(getClass().getResource('/visualization/xsl/xrechnung-html.xsl').getPath())
+        and: "a Transformer which creates a result"
+        def transformer = new XSLTransformAction()
+        transformer.check(bag)
+
+        and: "config with a Target to save to"
+        def transformationConfig = new TransformationConfiguration('', getClass().getResource('/visualization/').getPath()+'result.html', new ArrayList<String>())
+
+        and: "no file at the location"
+        def file = new File(getClass().getResource('/visualization/').getPath()+'result.html')
+        file.delete()
+
+        and: "A SourceSaver"
+        def sourceSaver = new ResultSaverAction(transformationConfig)
+        when: "the action is called"
+        sourceSaver.check(bag)
+
+        then: "A file should exist"
+        file.exists()
+        and: "equal the expected result"
+        def expectedFile = new File(getClass().getResource('/visualization/result/result.html').getPath())
+        Files.readAllLines(file.toPath()) == Files.readAllLines(expectedFile.toPath())
     }
 }
